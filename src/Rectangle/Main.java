12079f2133fe6618f6bc60e0fd50eb2af07ec78d
DoChuan13 <chuando.9213@gmail.com>
2023-03-02 21:20:46 +0700

    Create Rectangle Class

---
+++ src/Rectangle/Main.java
@@ -0,0 +1,17 @@
+package Rectangle;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the width: ");
+        double width = scanner.nextDouble();
+        System.out.print("Enter the height: ");
+        double height = scanner.nextDouble();
+        Rectangle rectangle = new Rectangle(width, height);
+        System.out.println("Your Rectangle \n" + rectangle.display());
+        System.out.println("Perimeter of the Rectangle: " + rectangle.getPerimeter());
+        System.out.println("Area of the Rectangle: " + rectangle.getArea());
+    }
+}
---
+++ src/Rectangle/Rectangle.java
@@ -0,0 +1,25 @@
+package Rectangle;
+
+public class Rectangle {
+    double width, height;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
+
+    public double getPerimeter() {
+        return (this.width * this.height) * 2;
+    }
+
+    public String display() {
+        return "Rectangle {" + "width = " + width + ", height = " + height + "}";
+    }
+}
